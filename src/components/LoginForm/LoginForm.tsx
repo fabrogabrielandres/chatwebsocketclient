f30792174dc2f8242e7e353eaac97879f127cec1
gabreil <fabrogabrielandres@gmail.com>
2025-08-12 20:02:29 +0200

    add zustand and axios

--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import type { User } from "../Chat/Chat";
+import { useAuthStore } from "../../stores/auth/auth.store";
 
 interface LoginFormProps {
   onLogin: (user: User) => void;
@@ -9,32 +10,42 @@ export const LoginForm = ({ onLogin }: LoginFormProps) => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
 
-  const isAuthenticated = async (username: string, password: string) => {
-    const response = await fetch("http://localhost:3001/api/auth", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        username,
-        password,
-      }),
-    });
-    const resp = await response.json();
-    return resp;
-  };
+  const loginUser = useAuthStore((state) => state.loginUser);
+
+  // const isAuthenticated = async (username: string, password: string) => {
+  //   const response = await fetch("http://localhost:3001/api/auth", {
+  //     method: "POST",
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //     },
+  //     body: JSON.stringify({
+  //       username,
+  //       password,
+  //     }),
+  //   });
+  //   const resp = await response.json();
+  //   return resp;
+  // };
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
 
-    const resp = await isAuthenticated(username, password);
-    console.log("user", resp);
+    // const resp = await isAuthenticated(username, password);
+    loginUser(username, password);
 
-    if (resp.user) {
-      onLogin(resp.user);
-    } else {
-      alert("Invalid credentials");
-    }
+    const user = {
+      id: "1",
+      username: "alice",
+      token: "alice-token",
+    };
+
+    onLogin({id: user.id, username: user.username, token: user.token, password: password });
+
+    // if (resp.user) {
+    //   onLogin(resp.user);
+    // } else {
+    //   alert("Invalid credentials");
+    // }
   };
 
   return (
---
+++ src/stores/auth/auth.store.ts
@@ -0,0 +1,54 @@
+import { devtools, persist } from "zustand/middleware";
+import type { AuthStatus } from "../../interfaces/auth.interface";
+import type { User } from "../../components/Chat/Chat";
+import { create, type StateCreator } from "zustand";
+import { AuthService } from "../../services/auth.service";
+
+export interface AuthState {
+  status: AuthStatus;
+  token?: string;
+  user?: User;
+
+  loginUser: (email: string, password: string) => Promise<void>;
+  checkAuthStatus: () => Promise<void>;
+  logoutUser: () => void;
+}
+
+const storeApi: StateCreator<AuthState> = (set) => ({
+  status: "pending",
+  token: undefined,
+  user: undefined,
+
+  loginUser: async (email: string, password: string) => {
+
+    try {
+      const { authorized, user } = await AuthService.login(email, password);
+      const token = user.token;      
+      set({ status: authorized, token, user });
+    } catch (error) {
+      console.error("Login failed:", error);
+      set({ status: "unauthorized", token: undefined, user: undefined });
+      throw "Unauthorized";
+    }
+  },
+
+  checkAuthStatus: async () => {
+    try {
+      const { authorized, user } = await AuthService.checkStatus();
+      const token = user.token;
+      set({ status: authorized, token, user });
+    } catch (error) {
+      console.log("Check status failed:", error);
+
+      set({ status: "unauthorized", token: undefined, user: undefined });
+    }
+  },
+
+  logoutUser: () => {
+    set({ status: "unauthorized", token: undefined, user: undefined });
+  },
+});
+
+export const useAuthStore = create<AuthState>()(
+  devtools(persist(storeApi, { name: "auth-storage" }))
+);
